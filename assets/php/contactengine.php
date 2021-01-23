7328da70a91b507af39b78c9858c076f423b58a6
Sethbarrie <sethbarrie@gmail.com>
2021-01-22 16:00:12 -0800

    Updated php script for contactme form

--- assets/php/contactengine.php
+++ assets/php/contactengine.php
@@ -1,43 +1,54 @@
 <?php
+  
+if($_POST) {
+    $name = "";
+    $email = "";
+    $subject = "";
+    $message = "";
+    $email_body = "<div>";
+      
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['subject'])) {
+        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$subject."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['body'])) {
+        $body = filter_var($_POST['body'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Concerned Department:</b></label>&nbsp;<span>".$body."</span>
+                        </div>";
+    }
 
-$EmailFrom = "chriscoyier@gmail.com";
-$EmailTo = "sethbarrie@gmail.com";
-$Subject = "Nice & Simple Contact Form by CSS-Tricks";
-$Name = Trim(stripslashes($_POST['Name'])); 
-$Tel = Trim(stripslashes($_POST['Tel'])); 
-$Email = Trim(stripslashes($_POST['Email'])); 
-$Message = Trim(stripslashes($_POST['Message'])); 
-
-// validation
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $Name;
-$Body .= "\n";
-$Body .= "Tel: ";
-$Body .= $Tel;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $Email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $Message;
-$Body .= "\n";
-
-// send email 
-$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
-
-// redirect to success page 
-if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
-}
-else{
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+    $email_body .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+      
+    if(mail("sethbarrie@gmail.com", $subject, $email_body, $headers)) {
+        echo "<p>Thank you for contacting us, $name. You will get a reply shortly.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
 }
 ?>
